b360335833c922e2642f275446da0dc722a9b132
Piotr Skamruk <piotr.skamruk@gmail.com>
2016-09-01 14:29:45 +0200

    compose: fix brackets and disconnect from host /run

--- contrib/docker/libvirt/start.sh
+++ contrib/docker/libvirt/start.sh
@@ -4,9 +4,9 @@ set -o errexit
 
 modprobe kvm || ((echo "Missing kvm module on host" && exit 1))
 if grep vmx /proc/cpuinfo &>/dev/null; then
-	modprobe kvm_intel || ((echo "Missing kvm_intel module on host" && exit 1))
+	modprobe kvm_intel || (echo "Missing kvm_intel module on host" && exit 1)
 elif grep svm /proc/cpuinfo &>/dev/null; then
-	modprobe kvm_amd || ((echo "Missing kvm_amd module on host" && exit 1))
+	modprobe kvm_amd || (echo "Missing kvm_amd module on host" && exit 1)
 fi
 
 chown root:root /etc/libvirt/libvirtd.conf
