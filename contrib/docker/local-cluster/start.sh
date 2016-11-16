02a1c7b75d7db4ce41636d7b01553feba142137e
Ivan Shvedunov <ishvedunov@mirantis.com>
2016-11-16 21:40:37 +0300

    Implement basic shell-based e2e test

---
+++ contrib/docker/local-cluster/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+ext_ip="$(ip route get 1 | awk '{print $NF;exit}')"
+export PATH=${PATH}:/go/src/k8s.io/kubernetes/third_party/etcd
+# make insecure port of apiserver accessible from outside
+export KUBELET_HOST="$ext_ip"
+export HOSTNAME_OVERRIDE="$ext_ip"
+export API_HOST="$ext_ip"
+export KUBERNETES_PROVIDER=local
+export CONTAINER_RUNTIME=remote
+export CONTAINER_RUNTIME_ENDPOINT=/run/virtlet.sock
+hack/local-up-cluster.sh -o _output/local/bin/linux/amd64
