c29134e43e6c9125cb173f8d3991259a401d0026
Ivan Shvedunov <ishvedunov@mirantis.com>
2016-11-25 22:09:30 +0300

    Add nettools.SetupBridge()

--- pkg/nettools/nettools.go
+++ pkg/nettools/nettools.go
@@ -1,6 +1,41 @@
+/*
+Copyright 2016 Mirantis
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Based on CNI's plugins/main/bridge/bridge.go, pkg/ip/link.go
+// Original copyright notice:
+//
+// Copyright 2014 CNI authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package nettools
 
 import (
+	"crypto/rand"
+	"errors"
 	"fmt"
 	"github.com/containernetworking/cni/pkg/ip"
 	"github.com/containernetworking/cni/pkg/ns"
@@ -34,4 +69,68 @@ func CreateEscapeVethPair(contNS ns.NetNS, ifName string, mtu int) (hostVeth, co
 	}
 
 	return
+}
+
+func createBridge(brName string, mtu int) (*netlink.Bridge, error) {
+	br := &netlink.Bridge{
+		LinkAttrs: netlink.LinkAttrs{
+			Name: brName,
+			MTU:  mtu,
+			// Let kernel use default txqueuelen; leaving it unset
+			// means 0, and a zero-length TX queue messes up FIFO
+			// traffic shapers which use TX queue length as the
+			// default packet limit
+			TxQLen: -1,
+		},
+	}
+
+	if err := netlink.LinkAdd(br); err != nil {
+		return nil, fmt.Errorf("could not add %q: %v", brName, err)
+	}
+
+	if err := netlink.LinkSetUp(br); err != nil {
+		return nil, err
+	}
+
+	return br, nil
+}
+
+// randomBridgeName returns string "br" with random suffix (hashed from entropy)
+func randomBridgeName() (string, error) {
+	entropy := make([]byte, 4)
+	if _, err := rand.Reader.Read(entropy); err != nil {
+		return "", fmt.Errorf("failed to generate random bridge name: %v", err)
+	}
+
+	return fmt.Sprintf("br%x", entropy), nil
+}
+
+// SetupBridge creates a bridge and adds specified links to it.
+// It sets bridge's MTU to MTU value of the first link.
+func SetupBridge(links []netlink.Link) (*netlink.Bridge, error) {
+	if len(links) == 0 {
+		return nil, errors.New("no links provided")
+	}
+
+	bridgeName, err := randomBridgeName()
+	if err != nil {
+		return nil, err
+	}
+
+	br, err := createBridge(bridgeName, links[0].Attrs().MTU)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create bridge %q: %v", bridgeName, err)
+	}
+
+	for _, link := range links {
+		if err = netlink.LinkSetMaster(link, br); err != nil {
+			delMessage := ""
+			if delErr := netlink.LinkDel(br); delErr != nil {
+				delMessage = fmt.Sprintf(" (and failed to delete the bridge: %v", err)
+			}
+			return nil, fmt.Errorf("failed to connect %q to bridge %v: %v%s", link.Attrs().Name, br.Attrs().Name, err, delMessage)
+		}
+	}
+
+	return br, nil
 }
--- pkg/nettools/nettools_test.go
+++ pkg/nettools/nettools_test.go
@@ -1,8 +1,26 @@
+/*
+Copyright 2016 Mirantis
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package nettools
 
 import (
+	"fmt"
 	"github.com/containernetworking/cni/pkg/ns"
 	"github.com/vishvananda/netlink"
+	"net"
 	"testing"
 )
 
@@ -55,6 +73,70 @@ func TestEscapePair(t *testing.T) {
 			}
 			if _, err = netlink.LinkByName(hostVeth.Attrs().Name); err == nil {
 				t.Errorf("host veth should not be present in container namespace")
+			}
+			return nil
+		})
+	})
+}
+
+func makeTestVeth(t *testing.T, base string, index int) netlink.Link {
+	name := fmt.Sprintf("%s%d", base, index)
+	veth := &netlink.Veth{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  name,
+			Flags: net.FlagUp,
+			MTU:   1500,
+		},
+		PeerName: "p" + name,
+	}
+	if err := netlink.LinkAdd(veth); err != nil {
+		t.Fatalf("failed to create veth: %v", err)
+	}
+
+	return veth
+}
+
+func makeTestBridge(t *testing.T, links []netlink.Link) *netlink.Bridge {
+	br, err := SetupBridge(links)
+	if err != nil {
+		t.Fatalf("failed to create first bridge: %v", err)
+	}
+
+	// re-retrieve link by name to be extra sure
+	if _, err := netlink.LinkByName(br.Attrs().Name); err != nil {
+		t.Fatalf("first bridge not found: %v", err)
+	}
+
+	return br
+}
+
+func TestSetupBridge(t *testing.T) {
+	withTempNetNS(t, func(hostNS ns.NetNS) {
+		hostNS.Do(func(ns.NetNS) error {
+			var links []netlink.Link
+			for i := 0; i < 4; i++ {
+				links = append(links, makeTestVeth(t, "veth", i))
+			}
+
+			brs := []*netlink.Bridge{
+				makeTestBridge(t, links[0:2]),
+				makeTestBridge(t, links[2:4]),
+			}
+			if brs[0].Attrs().Name == brs[1].Attrs().Name {
+				t.Errorf("bridges have identical name %q", brs[0].Attrs().Name)
+			}
+			if brs[0].Attrs().Index == brs[1].Attrs().Index {
+				t.Errorf("bridges have the same index %d", brs[0].Attrs().Index)
+			}
+
+			for i := 0; i < 4; i++ {
+				bridgeIndex := brs[i/2].Attrs().Index
+				name := links[i].Attrs().Name
+				if link, err := netlink.LinkByName(name); err != nil {
+					t.Errorf("cannot locate link %q", name)
+				} else if link.Attrs().MasterIndex != bridgeIndex {
+					t.Errorf("link %q doesn't belong to bridge %d", name, i/2)
+				}
 			}
 			return nil
 		})
