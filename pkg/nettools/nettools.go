7bfd96e895267ee93c434636a5812f4259330c03
Ivan Shvedunov <ishvedunov@mirantis.com>
2016-11-30 15:46:56 +0300

    Add SetupContainerSideNetwork() to nettools

--- pkg/nettools/nettools.go
+++ pkg/nettools/nettools.go
@@ -34,7 +34,6 @@ limitations under the License.
 package nettools
 
 import (
-	"crypto/rand"
 	"errors"
 	"fmt"
 	"github.com/containernetworking/cni/pkg/ip"
@@ -42,6 +41,13 @@ import (
 	"github.com/vishvananda/netlink"
 	"net"
 	"syscall"
+)
+
+const (
+	tapInterfaceName         = "tap0"
+	dhcpVethContainerEndName = "dhcpveth0"
+	dhcpVethDhcpEndName      = "dhcpveth1"
+	containerBridgeName      = "br0"
 )
 
 type Route struct {
@@ -54,30 +60,36 @@ type InterfaceInfo struct {
 	Routes []Route
 }
 
-// CreateEscapeVethPair creates a veth pair with contVeth residing in
-// the specified container network namespace and hostVeth residing in
-// the host namespace.
-func CreateEscapeVethPair(contNS ns.NetNS, ifName string, mtu int) (hostVeth, contVeth netlink.Link, err error) {
-	var hostVethName string
+type ContainerNetwork struct {
+	Info   *InterfaceInfo
+	DhcpNS ns.NetNS
+}
 
-	err = contNS.Do(func(hostNS ns.NetNS) error {
-		// create the veth pair in the container and move host end into host netns
-		hostVeth, contVeth, err = ip.SetupVeth(ifName, mtu, hostNS)
+// TBD: rm this
+// CreateEscapeVethPair creates a veth pair with innerVeth residing in
+// the specified network namespace innerNS and outerVeth residing in
+// the 'outer' (current) namespace.
+func CreateEscapeVethPair(innerNS ns.NetNS, ifName string, mtu int) (outerVeth, innerVeth netlink.Link, err error) {
+	var outerVethName string
+
+	err = innerNS.Do(func(outerNS ns.NetNS) error {
+		// create the veth pair in the inner ns and move outer end into the outer netns
+		outerVeth, innerVeth, err = ip.SetupVeth(ifName, mtu, outerNS)
 		if err != nil {
 			return err
 		}
 
-		hostVethName = hostVeth.Attrs().Name
+		outerVethName = outerVeth.Attrs().Name
 		return nil
 	})
 	if err != nil {
 		return
 	}
 
-	// need to lookup hostVeth again as its index has changed during ns move
-	hostVeth, err = netlink.LinkByName(hostVethName)
+	// need to lookup outerVeth again as its index has changed during ns move
+	outerVeth, err = netlink.LinkByName(outerVethName)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to lookup %q: %v", hostVethName, err)
+		return nil, nil, fmt.Errorf("failed to lookup %q: %v", outerVethName, err)
 	}
 
 	return
@@ -107,26 +119,11 @@ func createBridge(brName string, mtu int) (*netlink.Bridge, error) {
 	return br, nil
 }
 
-// randomBridgeName returns string "br" with random suffix (hashed from entropy)
-func randomBridgeName() (string, error) {
-	entropy := make([]byte, 4)
-	if _, err := rand.Reader.Read(entropy); err != nil {
-		return "", fmt.Errorf("failed to generate random bridge name: %v", err)
-	}
-
-	return fmt.Sprintf("br%x", entropy), nil
-}
-
 // SetupBridge creates a bridge and adds specified links to it.
 // It sets bridge's MTU to MTU value of the first link.
-func SetupBridge(links []netlink.Link) (*netlink.Bridge, error) {
+func SetupBridge(bridgeName string, links []netlink.Link) (*netlink.Bridge, error) {
 	if len(links) == 0 {
 		return nil, errors.New("no links provided")
-	}
-
-	bridgeName, err := randomBridgeName()
-	if err != nil {
-		return nil, err
 	}
 
 	br, err := createBridge(bridgeName, links[0].Attrs().MTU)
@@ -153,10 +150,11 @@ func SetupBridge(links []netlink.Link) (*netlink.Bridge, error) {
 // address from the namespace along with the routes.
 // There must be exactly one veth interface in the namespace
 // and exactly one address associated with veth.
-func GrabInterfaceInfo() (*InterfaceInfo, error) {
+// Returns interface info struct, original veth link and error, if any.
+func GrabInterfaceInfo() (*InterfaceInfo, netlink.Link, error) {
 	links, err := netlink.LinkList()
 	if err != nil {
-		return nil, fmt.Errorf("error listing links: %v", err)
+		return nil, nil, fmt.Errorf("error listing links: %v", err)
 	}
 	var veth netlink.Link
 	for _, link := range links {
@@ -164,20 +162,20 @@ func GrabInterfaceInfo() (*InterfaceInfo, error) {
 			continue
 		}
 		if veth != nil {
-			return nil, errors.New("multiple veth links detected in container namespace")
+			return nil, nil, errors.New("multiple veth links detected in container namespace")
 		}
 		veth = link
 	}
 	if veth == nil {
-		return nil, errors.New("no veth interface found")
+		return nil, nil, errors.New("no veth interface found")
 	}
 
 	addrs, err := netlink.AddrList(veth, netlink.FAMILY_V4)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get addresses for veth: %v", err)
+		return nil, nil, fmt.Errorf("failed to get addresses for veth: %v", err)
 	}
 	if len(addrs) != 1 {
-		return nil, fmt.Errorf("expected exactly one address for veth, but got %v", addrs)
+		return nil, nil, fmt.Errorf("expected exactly one address for veth, but got %v", addrs)
 	}
 
 	info := &InterfaceInfo{
@@ -187,7 +185,7 @@ func GrabInterfaceInfo() (*InterfaceInfo, error) {
 
 	routes, err := netlink.RouteList(veth, netlink.FAMILY_V4)
 	if err != nil {
-		return nil, fmt.Errorf("failed to list routes: %v", err)
+		return nil, nil, fmt.Errorf("failed to list routes: %v", err)
 	}
 	for _, route := range routes {
 		if route.Protocol == syscall.RTPROT_KERNEL {
@@ -203,15 +201,99 @@ func GrabInterfaceInfo() (*InterfaceInfo, error) {
 			Via:         route.Gw,
 		})
 		if err = netlink.RouteDel(&route); err != nil {
-			return nil, fmt.Errorf("error deleting route: %v", err)
+			return nil, nil, fmt.Errorf("error deleting route: %v", err)
 		}
 	}
 
 	for _, addr := range addrs {
 		if err = netlink.AddrDel(veth, &addr); err != nil {
-			return nil, fmt.Errorf("error deleting address from the route: %v", err)
+			return nil, nil, fmt.Errorf("error deleting address from the route: %v", err)
 		}
 	}
 
-	return info, nil
+	return info, veth, nil
+}
+
+// SetupContainerSideNetwork sets up networking in container namespace.
+// It does so by calling GrabInterfaceInfo() first, then making additional
+// network namespace for dhcp server and preparing the following network
+// interfaces:
+//   In container ns:
+//     tap0      - tap interface for the VM
+//     dhcpveth0 - container end of dchp veth pair
+//     br0       - a bridge that joins tap0, dhcpveth0 and original CNI veth
+//   In dchp server ns:
+//     dhcpveth1 - dhcp server end of dhcp veth pair
+// Returns container network desciption and error, if any
+func SetupContainerSideNetwork() (*ContainerNetwork, error) {
+	info, contVeth, err := GrabInterfaceInfo()
+	if err != nil {
+		return nil, err
+	}
+
+	dhcpNS, err := ns.NewNS()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create dhcp namespace: %v", err)
+	}
+
+	tap := &netlink.Tuntap{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  tapInterfaceName,
+			Flags: net.FlagUp,
+			MTU:   contVeth.Attrs().MTU,
+		},
+		Mode: netlink.TUNTAP_MODE_TAP,
+	}
+	if err := netlink.LinkAdd(tap); err != nil {
+		return nil, fmt.Errorf("failed to create tap interface: %v", err)
+	}
+	if err = netlink.LinkSetUp(tap); err != nil {
+		return nil, fmt.Errorf("failed to set %q up: %v", tapInterfaceName, err)
+	}
+
+	dhcpVeth := &netlink.Veth{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  dhcpVethContainerEndName,
+			Flags: net.FlagUp,
+			MTU:   contVeth.Attrs().MTU,
+		},
+		PeerName: dhcpVethDhcpEndName,
+	}
+	if err := netlink.LinkAdd(dhcpVeth); err != nil {
+		return nil, fmt.Errorf("failed to create dhcp veth pair: %v", err)
+	}
+	if err = netlink.LinkSetUp(dhcpVeth); err != nil {
+		return nil, fmt.Errorf("failed to set %q up: %v", dhcpVethContainerEndName, err)
+	}
+
+	dhcpSideVeth, err := netlink.LinkByName(dhcpVethDhcpEndName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find dhcp side veth before moving it: %v", err)
+	}
+
+	if err := netlink.LinkSetNsFd(dhcpSideVeth, int(dhcpNS.Fd())); err != nil {
+		return nil, fmt.Errorf("failed to move dhcp side veth to its ns: %v", err)
+	}
+
+	if err := dhcpNS.Do(func(ns.NetNS) error {
+		veth, err := netlink.LinkByName(dhcpVethDhcpEndName)
+		if err != nil {
+			return fmt.Errorf("failed to find dhcp side veth: %v", err)
+		}
+		if err = netlink.LinkSetUp(veth); err != nil {
+			return fmt.Errorf("failed to set %q up: %v", dhcpVethDhcpEndName, err)
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	if _, err := SetupBridge(containerBridgeName, []netlink.Link{contVeth, tap, dhcpVeth}); err != nil {
+		return nil, fmt.Errorf("failed to create bridge: %v", err)
+	}
+
+	return &ContainerNetwork{
+		Info:   info,
+		DhcpNS: dhcpNS,
+	}, nil
 }
