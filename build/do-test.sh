abe062d8e5847357c59b8fae688c53d987feb9dc
Ivan Shvedunov <ishvedunov@mirantis.com>
2016-12-21 23:12:50 +0300

    Dump libvirtd log upon integration test failure
    libvirtd appears to be dying for some reason

--- build/do-test.sh
+++ build/do-test.sh
@@ -9,7 +9,12 @@ set -o errtrace
 ./configure
 make
 make install
+
 if ! VIRTLET_DISABLE_KVM=1 make check; then
-    find . -name test-suite.log | xargs cat
+    find . -name test-suite.log | xargs cat >&2
+
+    echo >&2 "***** libvirtd.log *****"
+    cat /var/log/libvirt/libvirtd.log >&2
+
     exit 1
 fi
