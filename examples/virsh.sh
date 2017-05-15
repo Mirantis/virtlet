e701a7428823099933b1c10b44edf5cbfb5ae2d5
Ivan Shvedunov <ishvedunov@mirantis.com>
2017-05-15 21:04:53 +0300

    Support `@pod[:namespace]` notation in virsh.sh

--- examples/virsh.sh
+++ examples/virsh.sh
@@ -1,9 +1,48 @@
 #!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
 opts=
 if [[ ${1:-} = "console" ]]; then
   # using -it with `virsh list` causes it to use \r\n as line endings,
   # which makes it less useful
   opts="-it"
 fi
+args=("$@")
+
+function get_pod_domain_id {
+  # @pod[:namespace]
+  if [[ ! ( ${1} =~ ^@([^:]+)(:(.*))?$ ) ]]; then
+    echo "bad podspec ${1}" >&2
+    exit 1
+  fi
+
+  local pod_name="${BASH_REMATCH[1]}"
+  local namespace="${BASH_REMATCH[3]}"
+  local namespace_opts=""
+  if [[ ${namespace} ]]; then
+      namespace_opts="-o ${namespace}"
+  fi
+  kubectl get pod ${namespace_opts} "${pod_name}" \
+          -o 'jsonpath={.status.containerStatuses[0].containerID}-{.status.containerStatuses[0].name}'|sed 's/.*__//'
+}
+
+if [[ ${1:-} = "poddomain" ]]; then
+  if [[ $# != 2 ]]; then
+    echo "poddomain command requires @podname[:namespace]" >&2
+    exit 1
+  fi
+  get_pod_domain_id "${2}"
+  exit 0
+fi
+
+for ((n=0; n < ${#args[@]}; n++)); do
+  if [[ ${args[${n}]} =~ ^@ ]]; then
+    args[${n}]="$(get_pod_domain_id "${args[${n}]}")"
+  fi
+done
+
 pod=$(kubectl get pods -n kube-system -l runtime=virtlet -o name|head -1|sed 's@.*/@@')
-kubectl exec ${opts} -n kube-system "${pod}" -- virsh "$@"
+kubectl exec ${opts} -n kube-system "${pod}" -- virsh ${args[@]+"${args[@]}"}
