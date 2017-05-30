d483effeab0682c7b40afbf462241395e589953d
Ivan Shvedunov <ivan4th@gmail.com>
2017-05-30 23:34:13 +0300

    Add golden master testing package

---
+++ tests/gm/data.go
@@ -0,0 +1,92 @@
+/*
+Copyright 2017 Mirantis
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package gm
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"regexp"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/golang/glog"
+)
+
+const (
+	dataIndent = "  "
+)
+
+var badFilenameCharRx = regexp.MustCompile(`[^\w-]`)
+
+// GetFilenameForTest convers a Go test name to filename
+func GetFilenameForTest(testName string) (string, error) {
+	filename := strings.Replace(testName, "/", "__", -1)
+	filename = badFilenameCharRx.ReplaceAllString(filename, "_") + ".json"
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", fmt.Errorf("can't get current directory: %v", err)
+	}
+	return filepath.Join(wd, filename), nil
+}
+
+// WriteDataFile serializes the specified value into a data file
+func WriteDataFile(filename string, v interface{}) error {
+	out, err := json.MarshalIndent(v, "", dataIndent)
+	if err != nil {
+		return fmt.Errorf("failed to marshal data for %q: %v. Input:\n%s",
+			filename, err, spew.Sdump(v))
+	}
+	if err := ioutil.WriteFile(filename, out, 0777); err != nil {
+		return fmt.Errorf("error writing %q: %v", filename, err)
+	}
+	return nil
+}
+
+// DataFileDiffers compares the specified value against the stored data file
+func DataFileDiffers(filename string, v interface{}) (bool, error) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return true, nil
+		}
+		return false, fmt.Errorf("error reading %q: %v", filename, err)
+	}
+
+	var curData interface{}
+	if err := json.Unmarshal(content, &curData); err != nil {
+		glog.Warning("failed to unmarshal %q to JSON: %v", filename, err)
+		return true, nil
+	}
+
+	out, err := json.Marshal(v)
+	if err != nil {
+		return false, fmt.Errorf("failed to marshal data for %q: %v. Input:\n%s",
+			filename, err, spew.Sdump(v))
+	}
+
+	var newData interface{}
+	if err := json.Unmarshal(out, &newData); err != nil {
+		return false, fmt.Errorf("failed to unmarshal back marshalled value for %q: %v. JSON:\n%s\nOriginal data:\n%s",
+			filename, err, string(out), spew.Sdump(v))
+	}
+
+	return !reflect.DeepEqual(curData, newData), nil
+}
---
+++ tests/gm/git_test.go
@@ -0,0 +1,116 @@
+/*
+Copyright 2017 Mirantis
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package gm
+
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+const (
+	gitSetupCmd = `
+git config --global user.email 'foo@example.com' &&
+git config --global user.name 'foo' &&
+git init &&
+git add . &&
+git commit -m 'ok'`
+)
+
+func TestGitDiff(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "git-test")
+	if err != nil {
+		t.Fatalf("TempDir(): %v", err)
+	}
+	defer os.RemoveAll(tmpDir)
+	origWd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("os.Getwd(): %v", err)
+	}
+	defer os.Chdir(origWd)
+	if err := os.Chdir(tmpDir); err != nil {
+		t.Fatalf("os.Chdir(): %v", err)
+	}
+	if err := ioutil.WriteFile("samplefile", []byte("foobar"), 0777); err != nil {
+		t.Fatalf("ioutil.WriteFile(): %v")
+	}
+
+	out, err := exec.Command("/bin/sh", "-c", gitSetupCmd).CombinedOutput()
+	if err != nil {
+		t.Fatalf("git init failed: %v:\n%s", err, out)
+	}
+
+	diffOut, err := GitDiff(filepath.Join(tmpDir, "samplefile"))
+	switch {
+	case err != nil:
+		t.Errorf("GitDiff (unchanged): %v", err)
+	case diffOut != "":
+		t.Errorf("GitDiff (unchanged): unexpected diff:\n%s", diffOut)
+	}
+
+	if err := ioutil.WriteFile("samplefile", []byte("baz"), 0777); err != nil {
+		t.Fatalf("ioutil.WriteFile(): %v")
+	}
+
+	diffOut, err = GitDiff(filepath.Join(tmpDir, "samplefile"))
+	switch {
+	case err != nil:
+		t.Errorf("GitDiff (changed): %v", err)
+	case diffOut == "":
+		t.Errorf("GitDiff (changed): no diff")
+	case !strings.Contains(diffOut, "-foobar") || !strings.Contains(diffOut, "+baz"):
+		t.Errorf("GitDiff (changed): bad diff output:\n%s", diffOut)
+	}
+
+	out, err = exec.Command("git", "add", "samplefile").CombinedOutput()
+	if err != nil {
+		t.Fatalf("git add failed: %v:\n%s", err, out)
+	}
+
+	diffOut, err = GitDiff(filepath.Join(tmpDir, "samplefile"))
+	switch {
+	case err != nil:
+		t.Errorf("GitDiff (staged): %v", err)
+	case diffOut != "":
+		t.Errorf("GitDiff (staged): unexpected diff:\n%s", diffOut)
+	}
+
+	diffOut, err = GitDiff(filepath.Join(tmpDir, "samplefile"))
+	switch {
+	case err != nil:
+		t.Errorf("GitDiff (staged, other work dir): %v", err)
+	case diffOut != "":
+		t.Errorf("GitDiff (staged, other work dir): unexpected diff:\n%s", diffOut)
+	}
+
+	if err := ioutil.WriteFile("newfile", []byte("newcontent"), 0777); err != nil {
+		t.Fatalf("ioutil.WriteFile(): %v")
+	}
+
+	diffOut, err = GitDiff(filepath.Join(tmpDir, "newfile"))
+	switch {
+	case err != nil:
+		t.Errorf("GitDiff (new): %v", err)
+	case diffOut == "":
+		t.Errorf("GitDiff (new): no diff")
+	case !strings.Contains(diffOut, "<NEW FILE>") || !strings.Contains(diffOut, "newcontent"):
+		t.Errorf("GitDiff (new): bad diff output:\n%s", diffOut)
+	}
+}
