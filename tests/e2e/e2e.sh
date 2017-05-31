3967a2dbdf0f525df6558574bfac8132e078fe69
Victoria Efimova <vefimova@mirantis.com>
2017-05-31 08:40:07 +0000

    Divide cpu bandwidth limit equally among VM's vCPU threads

--- tests/e2e/e2e.sh
+++ tests/e2e/e2e.sh
@@ -132,7 +132,7 @@ fi
 
 # test changing vcpu count
 
-kubectl convert -f "${SCRIPT_DIR}/../../examples/cirros-vm.yaml" --local -o json | docker exec -i kube-master jq '.metadata.annotations.VirtletVCPUCount = "2"' | kubectl create -f -
+kubectl convert -f "${SCRIPT_DIR}/../../examples/cirros-vm.yaml" --local -o json | docker exec -i kube-master jq '.metadata.annotations.VirtletVCPUCount = "2" | .spec.containers[0].resources.limits.cpu = "500m"' | kubectl create -f -
 
 wait-for-pod cirros-vm
 
@@ -151,6 +151,26 @@ function domain_xpath {
 }
 
 pod_domain="$("${virsh}" poddomain @cirros-vm)"
+
+# <cputune>
+#    <period>100000</period>
+#    <quota>25000</quota>
+# </cputune>
+expected_dom_quota="25000"
+expected_dom_period="100000"
+
+dom_quota="$(domain_xpath "${pod_domain}" 'string(/domain/cputune/quota'))"
+dom_period="$(domain_xpath "${pod_domain}" 'string(/domain/cputune/period'))"
+
+if [[ ${dom_quota} != ${expected_dom_quota} ]]; then
+  echo "Bad quota value in the domain definition. Expected ${dom_quota}, but got ${expected_dom_quota}" >&2
+  exit 1
+fi
+
+if [[ ${dom_period} != ${expected_dom_period} ]]; then
+  echo "Bad period value in the domain definition. Expected ${dom_period}, but got ${expected_dom_period}" >&2
+  exit 1
+fi
 
 # <memory unit='KiB'>131072</memory>
 dom_mem_size_k="$(domain_xpath "${pod_domain}" 'string(/domain/memory[@unit="KiB"])')"
