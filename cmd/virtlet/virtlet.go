7f5e105978f639d2c855970c82bff4c90131d6b3
Ivan Shvedunov <ishvedunov@mirantis.com>
2017-06-28 19:59:53 +0300

    Fix Virtlet DB path (again)

--- cmd/virtlet/virtlet.go
+++ cmd/virtlet/virtlet.go
@@ -34,7 +34,7 @@ var (
 		"Storage pool in which the images should be stored")
 	storageBackend = flag.String("storage-backend", "dir",
 		"Libvirt storage pool type/backend")
-	boltPath = flag.String("bolt-path", "/var/data/virtlet/virtlet.db",
+	boltPath = flag.String("bolt-path", "/var/lib/virtlet/virtlet.db",
 		"Path to the bolt database file")
 	listen = flag.String("listen", "/run/virtlet.sock",
 		"The unix socket to listen on, e.g. /run/virtlet.sock")
