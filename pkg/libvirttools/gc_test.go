44b80bd6c5bac6be102f677c463beb4c60fcdc92
Piotr Skamruk <pskamruk@mirantis.com>
2017-08-14 13:17:37 +0200

    libvirttools: Add test for qcow2 flexvolumes GC

--- pkg/libvirttools/gc_test.go
+++ pkg/libvirttools/gc_test.go
@@ -111,3 +111,45 @@ func TestRootVolumesCleanup(t *testing.T) {
 
 	gm.Verify(t, ct.rec.Content())
 }
+
+func TestQcow2VolumesCleanup(t *testing.T) {
+	ct := newContainerTester(t, fake.NewToplevelRecorder())
+	defer ct.teardown()
+
+	pool, err := ct.storageConn.LookupStoragePoolByName("volumes")
+	if err != nil {
+		t.Fatalf("LookupStoragePoolByName did not find 'volumes': %v", err)
+	}
+
+	for _, uuid := range randomUUIDs {
+		if _, err := pool.CreateStorageVol(&libvirtxml.StorageVolume{
+			Name:   "qcow flexvolume for " + uuid,
+			Target: &libvirtxml.StorageVolumeTarget{Path: "/some/path/virtlet-" + uuid},
+		}); err != nil {
+			t.Fatalf("Cannot define new fake volume: %v", err)
+		}
+	}
+	if _, err := pool.CreateStorageVol(&libvirtxml.StorageVolume{
+		Name:   "some other volume",
+		Target: &libvirtxml.StorageVolumeTarget{Path: "/path/with/different/prefix"},
+	}); err != nil {
+		t.Fatalf("Cannot define new fake volume: %v", err)
+	}
+
+	if volumes, _ := pool.ListAllVolumes(); len(volumes) != 4 {
+		t.Errorf("Defined 4 fake volumes but ListAllVolumes() returned %d of them", len(volumes))
+	}
+
+	// this should remove only qcow2 volumes for two first elements of randomUUIDs slice
+	// keeping intact other
+	errors := ct.virtTool.removeOrphanQcow2Volumes(randomUUIDs[2:])
+	if errors != nil {
+		t.Errorf("removeOrphanRootVolumes returned errors: %v", errors)
+	}
+
+	if volumes, _ := pool.ListAllVolumes(); len(volumes) != 2 {
+		t.Errorf("After calling removeOrphanQcow2Volumes expected two remaining volumes, ListAllVolumes() returned %d of them", len(volumes))
+	}
+
+	gm.Verify(t, ct.rec.Content())
+}
