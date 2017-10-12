20f7e71369a978d203ecda13cf222e9950ac06d4
Ivan Shvedunov <ishvedunov@mirantis.com>
2017-10-13 00:43:43 +0300

    Always run CNI plugins in the host netns

--- images/image_skel/start.sh
+++ images/image_skel/start.sh
@@ -29,4 +29,14 @@ while [ ! -S /var/run/libvirt/libvirt-sock ] ; do
   sleep 0.3
 done
 
+# FIXME: make tapfdsource do netns stuff in a separate process
+if [ -d /opt/cni/bin.orig ]; then
+  mkdir /opt/cni/bin
+  find /opt/cni/bin.orig -maxdepth 1 -executable -type f | while read p; do
+    newname="/opt/cni/bin/$(basename "$p")"
+    echo -e "#!/bin/sh\nexec /usr/bin/nsenter -t 1 -n ${p} \"\$@\"" >"${newname}"
+    chmod +x "${newname}"
+  done
+fi
+
 /usr/local/bin/virtlet -v=${VIRTLET_LOGLEVEL:-3} -logtostderr=true -image-download-protocol="${PROTOCOL}" -image-translations-dir="${IMAGE_TRANSLATIONS_DIR}" "${RAW_DEVICES}"
