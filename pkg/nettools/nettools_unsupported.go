32aa26f702b5865e5e5fa012b6e969e074bbe98b
Ivan Shvedunov <ivan4th@gmail.com>
2018-01-09 21:21:09 +0300

    Make pkg/nettools compilable on Mac OS X

---
+++ pkg/nettools/nettools_unsupported.go
@@ -0,0 +1,29 @@
+// +build !linux
+
+/*
+Copyright 2018 Mirantis
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package nettools
+
+// Some missing constants that break syntax check
+// on non-Linux systems
+const (
+	FAMILY_ALL     = 0
+	FAMILY_V4      = 0
+	RTPROT_KERNEL  = 0
+	SCOPE_LINK     = 0
+	SCOPE_UNIVERSE = 0
+)
