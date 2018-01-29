f5cdcbed0d5c62f5babcc1eb7571ddd732c0b59d
Ivan Shvedunov <ivan4th@gmail.com>
2018-01-29 21:08:58 +0300

    Add RunProcess to testutils

--- pkg/stream/utils_test.go
+++ pkg/stream/utils_test.go
@@ -20,10 +20,11 @@ import (
 	"io/ioutil"
 	"net"
 	"os"
-	"os/exec"
 	"runtime"
 	"testing"
 	"time"
+
+	testutils "github.com/Mirantis/virtlet/pkg/utils/testing"
 )
 
 func TestGetPidFromConnection(t *testing.T) {
@@ -34,12 +35,10 @@ func TestGetPidFromConnection(t *testing.T) {
 	defer os.Remove(socket.Name())
 	socketPath := socket.Name()
 
-	cmd := exec.Command("nc", "-l", "-U", socketPath)
-	if err := cmd.Start(); err != nil {
-		t.Errorf("Error when starting command: %v", err)
-	}
-	defer cmd.Process.Kill()
-	//wait for nc to start
+	tc := testutils.RunProcess(t, "nc", []string{"-l", "-U", socketPath}, nil)
+	defer tc.Stop()
+
+	// wait for nc to start
 	time.Sleep(2 * time.Second)
 
 	conn, err := net.DialUnix("unix", nil, &net.UnixAddr{socketPath, "unix"})
@@ -52,8 +51,8 @@ func TestGetPidFromConnection(t *testing.T) {
 	if err != nil {
 		t.Errorf("Couldn't get pid from Unix socket: %v", err)
 	}
-	if pid != int32(cmd.Process.Pid) {
-		t.Errorf("Wrong pid from getPidFromConnection. Expected: %d, got %d", cmd.Process.Pid, pid)
+	if pid != int32(tc.Pid()) {
+		t.Errorf("Wrong pid from getPidFromConnection. Expected: %d, got %d", tc.Pid(), pid)
 	}
 }
 
@@ -61,16 +60,13 @@ func TestGetProcessEnvironment(t *testing.T) {
 	if runtime.GOOS != "linux" {
 		t.Skip("getProcessEnvironment only works on Linux")
 	}
-	cmd := exec.Command("sleep", "10")
-	cmd.Env = append(os.Environ(),
+
+	tc := testutils.RunProcess(t, "sleep", []string{"10"}, []string{
 		"FOO=1",
 		"BAR=asd",
-	)
-	if err := cmd.Start(); err != nil {
-		t.Errorf("Error when starting command: %v", err)
-	}
-	defer cmd.Process.Kill()
-	env, err := getProcessEnvironment(int32(cmd.Process.Pid))
+	})
+	defer tc.Stop()
+	env, err := getProcessEnvironment(int32(tc.Pid()))
 	if err != nil {
 		t.Error(err)
 	}
