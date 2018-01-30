6d10ad2e4a0ce93b29808c0a6dd3833db7c9860a
Ivan Shvedunov <ivan4th@gmail.com>
2018-01-30 18:32:58 +0300

    Fix nil arg handling in NsFix

--- pkg/utils/nsfix_test.go
+++ pkg/utils/nsfix_test.go
@@ -78,7 +78,16 @@ func handleNsFixTest2(data interface{}) (interface{}, error) {
 	return nil, nil
 }
 
-func TestNsFix(t *testing.T) {
+func handleNsFixWithNilArg(data interface{}) (interface{}, error) {
+	return 42, nil
+}
+
+func handleNsFixWithNilResult(data interface{}) (interface{}, error) {
+	targetPath := data.(*string)
+	return nil, os.Mkdir(filepath.Join(*targetPath, "foobar"), 0777)
+}
+
+func verifyNsFix(t *testing.T, toRun func(tmpDir string, dirs map[string]string, pids []int)) {
 	if runtime.GOOS != "linux" {
 		t.Skip("The namespace fix only works on Linux")
 	}
@@ -96,21 +105,29 @@ func TestNsFix(t *testing.T) {
 		t.Fatalf("Chown(): %v", err)
 	}
 
+	// TEST_NSFIX is handled by init() below
+	os.Setenv("TEST_NSFIX", "1")
+	defer os.Setenv("TEST_NSFIX", "")
+
 	dirA := filepath.Join(tmpDir, "a")
-	dirB := filepath.Join(tmpDir, "b")
-	dirC := filepath.Join(dirA, "c")
 	dirD := filepath.Join(tmpDir, "d")
-	dirE := filepath.Join(dirD, "e")
-	for _, p := range []string{dirA, dirB, dirC, dirD, dirE} {
-		if err := os.Mkdir(p, 0777); err != nil {
+	dirs := map[string]string{
+		"a": dirA,
+		"b": filepath.Join(tmpDir, "b"),
+		"c": filepath.Join(dirA, "c"),
+		"d": dirD,
+		"e": filepath.Join(dirD, "e"),
+	}
+	for _, p := range []string{"a", "b", "c", "d", "e"} {
+		if err := os.Mkdir(dirs[p], 0777); err != nil {
 			t.Fatalf("Can't create dir %q: %v", p, err)
 		}
 	}
 
 	var pids []int
 	for _, cmd := range []string{
-		fmt.Sprintf("mount --bind %q %q && sleep 10000", dirA, dirB),
-		fmt.Sprintf("mount --bind %q %q && sleep 10000", dirD, dirB),
+		fmt.Sprintf("mount --bind %q %q && sleep 10000", dirs["a"], dirs["b"]),
+		fmt.Sprintf("mount --bind %q %q && sleep 10000", dirs["d"], dirs["b"]),
 	} {
 		tc := testutils.RunProcess(t, "unshare", []string{
 			"-m", "/bin/bash", "-c", cmd,
@@ -119,57 +136,84 @@ func TestNsFix(t *testing.T) {
 		pids = append(pids, tc.Pid())
 	}
 
-	// TEST_NSFIX is handled by init() below
-	os.Setenv("TEST_NSFIX", "1")
-	defer os.Setenv("TEST_NSFIX", "")
+	toRun(tmpDir, dirs, pids)
+}
 
-	var r nsFixTestRet
-	if err := SpawnInNamespaces(pids[0], "nsfixtest1", nsFixTestArg{dirB}, false, &r); err != nil {
-		t.Fatalf("SpawnInNamespaces(): %v", err)
-	}
-	resultStr := strings.Join(r.Files, "\n")
-	expectedResultStr := "c"
-	if resultStr != expectedResultStr {
-		t.Errorf("Bad result from SpawnInNamespaces(): %q instead of %q", resultStr, expectedResultStr)
-	}
-	if !r.IsRoot {
-		t.Errorf("SpawnInNamespaces dropped privs when not requested to do so")
-	}
-
-	if err := SpawnInNamespaces(pids[1], "nsfixtest1", nsFixTestArg{dirB}, true, &r); err != nil {
-		t.Fatalf("SpawnInNamespaces(): %v", err)
-	}
-	resultStr = strings.Join(r.Files, "\n")
-	expectedResultStr = "e"
-	if resultStr != expectedResultStr {
-		t.Errorf("Bad result from SpawnInNamespaces(): %q instead of %q", resultStr, expectedResultStr)
-	}
-	if r.IsRoot {
-		t.Errorf("SpawnInNamespaces didn't drop privs not requested to do so")
-	}
-
-	// we examine SwitchToNamespace in the same test to make
-	// sure the calls don't interfere between themselves
-	cmd := exec.Command(os.Args[0])
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	cmd.Env = append(os.Environ(), fmt.Sprintf("TEST_NSFIX_SWITCH=%d:%s", pids[1], dirB))
-	if err := cmd.Run(); err != nil {
-		if exitErr, ok := err.(*exec.ExitError); ok {
-			t.Errorf("Error rerunning the test executable: %v\nstderr:\n%s", err, exitErr.Stderr)
-		} else {
-			t.Errorf("Error rerunning the test executable: %v", err)
+func TestNsFix(t *testing.T) {
+	verifyNsFix(t, func(tmpDir string, dirs map[string]string, pids []int) {
+		var r nsFixTestRet
+		if err := SpawnInNamespaces(pids[0], "nsFixTest1", nsFixTestArg{dirs["b"]}, false, &r); err != nil {
+			t.Fatalf("SpawnInNamespaces(): %v", err)
 		}
-	}
+		resultStr := strings.Join(r.Files, "\n")
+		expectedResultStr := "c"
+		if resultStr != expectedResultStr {
+			t.Errorf("Bad result from SpawnInNamespaces(): %q instead of %q", resultStr, expectedResultStr)
+		}
+		if !r.IsRoot {
+			t.Errorf("SpawnInNamespaces dropped privs when not requested to do so")
+		}
 
-	outPath := filepath.Join(dirD, "out")
-	expectedContents := "e"
-	switch outStr, err := ioutil.ReadFile(outPath); {
-	case err != nil:
-		t.Errorf("Error reading %q: %v", outPath, err)
-	case string(outStr) != expectedContents:
-		t.Errorf("bad out file contents: %q instead of %q", outStr, expectedContents)
-	}
+		if err := SpawnInNamespaces(pids[1], "nsFixTest1", nsFixTestArg{dirs["b"]}, true, &r); err != nil {
+			t.Fatalf("SpawnInNamespaces(): %v", err)
+		}
+		resultStr = strings.Join(r.Files, "\n")
+		expectedResultStr = "e"
+		if resultStr != expectedResultStr {
+			t.Errorf("Bad result from SpawnInNamespaces(): %q instead of %q", resultStr, expectedResultStr)
+		}
+		if r.IsRoot {
+			t.Errorf("SpawnInNamespaces didn't drop privs not requested to do so")
+		}
+
+		// we examine SwitchToNamespace in the same test to make
+		// sure the calls don't interfere between themselves
+		cmd := exec.Command(os.Args[0])
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		cmd.Env = append(os.Environ(), fmt.Sprintf("TEST_NSFIX_SWITCH=%d:%s", pids[1], dirs["b"]))
+		if err := cmd.Run(); err != nil {
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				t.Errorf("Error rerunning the test executable: %v\nstderr:\n%s", err, exitErr.Stderr)
+			} else {
+				t.Errorf("Error rerunning the test executable: %v", err)
+			}
+		}
+
+		outPath := filepath.Join(dirs["d"], "out")
+		expectedContents := "e"
+		switch outStr, err := ioutil.ReadFile(outPath); {
+		case err != nil:
+			t.Errorf("Error reading %q: %v", outPath, err)
+		case string(outStr) != expectedContents:
+			t.Errorf("bad out file contents: %q instead of %q", outStr, expectedContents)
+		}
+	})
+}
+
+func TestNsFixWithNilArg(t *testing.T) {
+	verifyNsFix(t, func(tmpDir string, dirs map[string]string, pids []int) {
+		var r int
+		if err := SpawnInNamespaces(pids[0], "nsFixTestNilArg", nil, false, &r); err != nil {
+			t.Fatalf("SpawnInNamespaces(): %v", err)
+		}
+		expectedResult := 42
+		if r != expectedResult {
+			t.Errorf("Bad result from SpawnInNamespaces(): %d instead of %d", r, expectedResult)
+		}
+	})
+}
+
+func TestNsFixWithNilResult(t *testing.T) {
+	verifyNsFix(t, func(tmpDir string, dirs map[string]string, pids []int) {
+		if err := SpawnInNamespaces(pids[0], "nsFixTestNilResult", dirs["b"], false, nil); err != nil {
+			t.Fatalf("SpawnInNamespaces(): %v", err)
+		}
+		// dirs["a"] is bind-mounted under dirs["b"]
+		if _, err := os.Stat(filepath.Join(dirs["a"], "foobar")); err != nil {
+			t.Errorf("Stat(): %v", err)
+		}
+	})
 }
 
 func init() {
@@ -183,12 +227,14 @@ func init() {
 		if err != nil {
 			glog.Fatalf("bad TEST_NSFIX_SWITCH: %q", switchStr)
 		}
-		if err := SwitchToNamespaces(pid, "nsfixtest2", nsFixTestArg{parts[1]}, false); err != nil {
+		if err := SwitchToNamespaces(pid, "nsFixTest2", nsFixTestArg{parts[1]}, false); err != nil {
 			glog.Fatalf("SwitchToNamespace(): %v", err)
 		}
 	}
-	RegisterNsFixReexec("nsfixtest1", handleNsFixTest1, nsFixTestArg{})
-	RegisterNsFixReexec("nsfixtest2", handleNsFixTest2, nsFixTestArg{})
+	RegisterNsFixReexec("nsFixTest1", handleNsFixTest1, nsFixTestArg{})
+	RegisterNsFixReexec("nsFixTest2", handleNsFixTest2, nsFixTestArg{})
+	RegisterNsFixReexec("nsFixTestNilArg", handleNsFixWithNilArg, nil)
+	RegisterNsFixReexec("nsFixTestNilResult", handleNsFixWithNilResult, "")
 	if os.Getenv("TEST_NSFIX") != "" {
 		// NOTE: this is not a recommended way to invoke
 		// reexec, but may be the easiest one for testing
