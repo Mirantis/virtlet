5680d57daa3621cda047fbb1113b53ac95e3ebfa
Piotr Skamruk <pskamruk@mirantis.com>
2018-02-28 15:30:52 +0100

    Implementation of data dump for metadata

--- pkg/tools/dumpmetadata.go
+++ pkg/tools/dumpmetadata.go
@@ -20,15 +20,22 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
 
+	"github.com/davecgh/go-spew/spew"
 	typedv1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	v1 "k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/rest"
+
+	"github.com/Mirantis/virtlet/pkg/metadata"
 )
 
 const (
 	// TODO: pass that as command line arg and use make default as constant
 	// used there and in cmd/virtlet/virtlet.go
-	virtletDBPath = "/var/lib/virtlet/virtlet.db"
+	virtletDBPath       = "/var/lib/virtlet/virtlet.db"
+	defaultIndentString = " "
 )
 
 // DumpMetadata contains data needed by dump-metedata subcommand.
@@ -56,38 +63,119 @@ func (d DumpMetadata) Run(clientset *typedv1.CoreV1Client, config *rest.Config, 
 	}
 
 	for _, pod := range pods {
-		stdout := &bytes.Buffer{}
-		stderr := &bytes.Buffer{}
-		stdin := bytes.NewBufferString("")
-
-		fmt.Printf("pod: %s\n", pod.Name)
-		exitCode, err := d.ExecCommandOnContainer(
-			pod.Name,
-			"virtlet",
-			"kube-system",
-			stdin,
-			stdout,
-			stderr,
-			"/bin/cat",
-			virtletDBPath,
-		)
+		fname, err := d.copyOutFile(&pod)
 		if err != nil {
-			fmt.Printf("  Error during downloading virtled metadata database: %v\n", err)
-		}
-		if exitCode != 0 {
-			fmt.Printf(" Got different than expected exit code: %d\n", exitCode)
-			fmt.Printf(" Remote command error output: %s\n", stderr.String())
+			fmt.Fprintf(os.Stderr, "Can't extract metadata db for pod %q: %v\n", pod.Name, err)
 			continue
 		}
-		f, err := ioutil.TempFile("/tmp", "virtlet-")
-		defer f.Close()
-		if err != nil {
-			fmt.Printf(" Got error during opening tempfile: %v\n", err)
-			continue
+		defer os.Remove(fname)
+		if err := dumpMetadata(fname); err != nil {
+			fmt.Fprintf(os.Stderr, "Can't dump metadata for pod %q: %v\n", pod.Name, err)
 		}
-		f.Write(stdout.Bytes())
-		fmt.Printf("  Virtlet metadata database for this pod saved in %q location\n", f.Name())
 	}
 
 	return nil
+}
+
+func (d *DumpMetadata) copyOutFile(pod *v1.Pod) (string, error) {
+	fmt.Printf("Virtlet pod name: %s\n", pod.Name)
+
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	stdin := bytes.NewBufferString("")
+
+	exitCode, err := d.ExecInContainer(
+		pod.Name,
+		"virtlet",
+		"kube-system",
+		stdin,
+		stdout,
+		stderr,
+		"/bin/cat",
+		virtletDBPath,
+	)
+	if err != nil {
+		return "", err
+	}
+
+	if exitCode != 0 {
+		return "", fmt.Errorf("remote command exit code was %d and its stderr output was:\n%s", exitCode, stderr.String())
+	}
+
+	f, err := ioutil.TempFile("/tmp", "virtlet-")
+	defer f.Close()
+	if err != nil {
+		return "", fmt.Errorf("error during opening tempfile: %v\n", err)
+	}
+	f.Write(stdout.Bytes())
+
+	return f.Name(), nil
+}
+
+func dumpMetadata(fname string) error {
+	s, err := metadata.NewMetadataStore(fname)
+	if err != nil {
+		return fmt.Errorf("can't open metadata db: %v", err)
+	}
+
+	printlnIndented(1, "Sandboxes:")
+	sandboxes, err := s.ListPodSandboxes(nil)
+	if err != nil {
+		return fmt.Errorf("can't list sandboxes: %v", err)
+	}
+
+	for _, smeta := range sandboxes {
+		if sinfo, err := smeta.Retrieve(); err != nil {
+			return fmt.Errorf("can't retrieve sandbox: %v", err)
+		} else if err := dumpSandbox(smeta.GetID(), sinfo, s); err != nil {
+			return fmt.Errorf("can't dump sandbox: %v", err)
+		}
+	}
+
+	printlnIndented(1, "Images:")
+	images, err := s.ImagesInUse()
+	if err != nil {
+		return fmt.Errorf("can't dump images: %v", err)
+	}
+
+	for image := range images {
+		printlnIndented(2, image)
+	}
+
+	return nil
+}
+
+func dumpSandbox(podid string, sandbox *metadata.PodSandboxInfo, s metadata.MetadataStore) error {
+	printlnIndented(2, "Sandbox id: %s", podid)
+	printlnIndented(0, spew.Sdump(sandbox))
+
+	printlnIndented(3, "Containers:")
+	containers, err := s.ListPodContainers(podid)
+	if err != nil {
+		return fmt.Errorf("can't retrieve list of containers: %v", err)
+	}
+
+	for _, cmeta := range containers {
+		printIndented(4, "Container id: %s", cmeta.GetID())
+		if cinfo, err := cmeta.Retrieve(); err != nil {
+			return fmt.Errorf("can't retrieve container metadata: %v", err)
+		} else {
+			printIndented(0, spew.Sdump(cinfo))
+		}
+	}
+
+	return nil
+}
+
+func printlnIndented(level int, format string, data ...interface{}) {
+	printIndented(level, format+"\n", data...)
+}
+
+func printIndented(level int, format string, data ...interface{}) {
+	printIndentedWith(defaultIndentString, 2*level, format, data...)
+}
+
+func printIndentedWith(with string, length int, format string, data ...interface{}) {
+	indentString := strings.Repeat(with, (length/len(with))+1)[:length]
+	fmt.Printf("%s%s", indentString, fmt.Sprintf(format, data...))
 }
