698cfb7e9b5977ecac6cb3684f3f60161e0e94c1
Ivan Shvedunov <ivan4th@gmail.com>
2018-03-29 14:45:15 +0300

    Use virtletctl gen in demo.sh

--- deploy/demo.sh
+++ deploy/demo.sh
@@ -270,8 +270,6 @@ function demo::get-correct-virtlet-release {
 
 function demo::start-virtlet {
   local -a virtlet_config=(--from-literal=download_protocol=http --from-literal=image_regexp_translation="$IMAGE_REGEXP_TRANSLATION")
-  local ds_location
-  local replace_image_in_ds
   if [[ ${VIRTLET_DEMO_BRANCH} ]]; then
     if [[ ${VIRTLET_DEMO_BRANCH} = "master" ]]; then
       virtlet_release="master"
@@ -281,8 +279,6 @@ function demo::start-virtlet {
       virtlet_docker_tag=$(echo $VIRTLET_DEMO_BRANCH | sed -e "s/\//_/g")
       BASE_LOCATION="https://raw.githubusercontent.com/Mirantis/virtlet/${virtlet_release}/"
     fi
-    ds_location="${BASE_LOCATION}/deploy/virtlet-ds.yaml"
-    replace_image_in_ds=1
   else
     if [[ ${VIRTLET_DEMO_RELEASE} ]]; then
       virtlet_release="${VIRTLET_DEMO_RELEASE}"
@@ -291,9 +287,7 @@ function demo::start-virtlet {
     fi
     # set correct urls and names
     virtlet_docker_tag="${virtlet_release}"
-    ds_location="${RELEASE_LOCATION}/${virtlet_release}/virtlet-ds.yaml"
     BASE_LOCATION="https://raw.githubusercontent.com/Mirantis/virtlet/${virtlet_release}/"
-    replace_image_in_ds=0
   fi
   echo "Will run demo using Virtlet:${virtlet_release} for demo and ${virtlet_docker_tag} as docker tag"
   if demo::kvm-ok; then
@@ -319,17 +313,9 @@ function demo::start-virtlet {
     fi
   fi
 
-  if [[ "${replace_image_in_ds}" == 1 ]]; then
-    demo::step "Deploying Virtlet DaemonSet from ${ds_location} and will set correct docker tag: ${virtlet_docker_tag}"
-    if [[ ${BASE_LOCATION} == https://* ]]; then
-      curl --silent "${ds_location}" | sed "s/image: mirantis\/virtlet/image: mirantis\/virtlet:${virtlet_docker_tag}/g" | "${kubectl}" create -f -
-    else
-      sed "s/image: mirantis\/virtlet/image: mirantis\/virtlet:${virtlet_docker_tag}/g" "${ds_location}" | "${kubectl}" create -f -
-    fi
-  else
-    demo::step "Deploying Virtlet DaemonSet from ${ds_location}"
-   "${kubectl}" create -f "${ds_location}"
-  fi
+  demo::step "Deploying Virtlet DaemonSet with docker tag ${virtlet_docker_tag}"
+  docker run --rm "mirantis/virtlet:${virtlet_docker_tag}" virtletctl gen --tag "${virtlet_docker_tag}" |
+      "${kubectl}" apply -f -
   demo::wait-for "Virtlet DaemonSet" demo::pods-ready runtime=virtlet
 }
 
