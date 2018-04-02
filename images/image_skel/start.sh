319d2594a705502047b20e53a1a8d18409583e60
Ivan Shvedunov <ivan4th@gmail.com>
2018-04-03 01:42:47 +0300

    Add raw_devices setting to Virtlet ConfigMap

--- images/image_skel/start.sh
+++ images/image_skel/start.sh
@@ -8,18 +8,17 @@ if [[ -f /dind/virtlet ]]; then
   ln -fs /dind/virtlet /usr/local/bin/virtlet
 fi
 
-if [[ ${VIRTLET_RAW_DEVICES:-} ]]; then
-  RAW_DEVICES="-raw-devices $VIRTLET_RAW_DEVICES"
-else
-  RAW_DEVICES=""
-fi
-
 PROTOCOL="${VIRTLET_DOWNLOAD_PROTOCOL:-https}"
 IMAGE_TRANSLATIONS_DIR="${IMAGE_TRANSLATIONS_DIR:-}"
+
+opts=(-v=${VIRTLET_LOGLEVEL:-3} -logtostderr=true -image-download-protocol="${PROTOCOL}" -image-translations-dir="${IMAGE_TRANSLATIONS_DIR}")
+if [[ ${VIRTLET_RAW_DEVICES:-} ]]; then
+  opts+=(-raw-devices "${VIRTLET_RAW_DEVICES}")
+fi
 
 while [ ! -S /var/run/libvirt/libvirt-sock ] ; do
   echo >&1 "Waiting for libvirt..."
   sleep 0.3
 done
 
-/usr/local/bin/virtlet -v=${VIRTLET_LOGLEVEL:-3} -logtostderr=true -image-download-protocol="${PROTOCOL}" -image-translations-dir="${IMAGE_TRANSLATIONS_DIR}" "${RAW_DEVICES}"
+/usr/local/bin/virtlet "${opts[@]}"
