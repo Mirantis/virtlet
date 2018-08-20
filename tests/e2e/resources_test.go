cef2ccc2aededad99f5005f9b03f87220bfe6564
Ivan Shvedunov <ishvedunov@mirantis.com>
2018-08-20 15:54:20 +0300

    Fix growroot race in e2e

--- tests/e2e/resources_test.go
+++ tests/e2e/resources_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package e2e
 
 import (
+	"fmt"
 	"regexp"
 	"strconv"
 	"time"
@@ -65,12 +66,17 @@ var _ = Describe("VM resources", func() {
 	})
 
 	It("Should grow the root volume size if requested", func() {
-		sizeStr := do(framework.RunSimple(ssh, "/bin/sh", "-c", `df -m / | tail -1 | awk "{print \$2}"`)).(string)
-		size, err := strconv.Atoi(sizeStr)
-		Expect(err).NotTo(HaveOccurred())
-		// FIXME: apparently growroot has problems even in CirrOS 0.4.0
-		// The check below should be something like '> 3900'
-		// but this caused a flake once with actual size being 3506 Mb.
-		Expect(size).To(BeNumerically(">", 3500))
+		Eventually(func() error {
+			sizeStr := do(framework.RunSimple(ssh, "/bin/sh", "-c", `df -m / | tail -1 | awk "{print \$2}"`)).(string)
+			size, err := strconv.Atoi(sizeStr)
+			if err != nil {
+				return err
+			}
+			minSize := 3900
+			if size < minSize {
+				return fmt.Errorf("the size is %d but needs to be at least %d", size, minSize)
+			}
+			return nil
+		})
 	})
 })
